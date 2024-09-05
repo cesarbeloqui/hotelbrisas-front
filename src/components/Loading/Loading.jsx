e2099659b1f3fdf60e7ae7471e2885dfc87fae38
cesarbeloqui <beloqui.cesar@gmail.com>
2024-09-05 17:07:52 -0300

    fgdfgdf

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,6 +1,6 @@
 // Logo en SVG
 import "./index.css";
-import {logoFooter} from "@/media/media";
+import { logoFooter } from "@/media/media";
 
 const LogoSVG = ({ className }) => (
   <div dangerouslySetInnerHTML={{ __html: logoFooter }} className={className} />
@@ -10,7 +10,7 @@ const LogoSVG = ({ className }) => (
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-[100vh] w-custom fixed bg-beige z-[60]">
+    <div className="flex items-center justify-center h-[100vh] w-[100vw] fixed bg-beige z-[60]">
       <span className="relative flex h-60 w-60">
         <span className="animate-ping-2 absolute h-full w-full rounded-full bg-beige opacity-75"></span>
         <span className="relative flex items-center justify-center rounded-full h-60 w-60 bg-fondoMarron font-cutive">
