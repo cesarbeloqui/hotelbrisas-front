8a0390f710746591cc86a93a08a35e493322413d
cesarbeloqui <beloqui.cesar@gmail.com>
2024-09-10 14:17:44 -0300

    sfgsd

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -14,8 +14,8 @@ const Loading = () => {
   return (
     <div className="flex items-center justify-center h-[100vh] w-[100vw] fixed bg-beige z-[60]">
       <span className="relative flex h-60 w-60">
-        <span className="animate-ping-2 absolute h-full w-full rounded-full bg-beige opacity-75"></span>
-        <span className="relative flex items-center justify-center rounded-full h-60 w-60 bg-fondoMarron font-cutive">
+        <span className=" absolute h-full w-full rounded-full bg-beige opacity-75"></span>
+        <span className="fade-in-out relative flex items-center justify-center rounded-full h-60 w-60 bg-fondoMarron font-cutive">
           <LogoSVG className="w-60 h-60" />
         </span>
       </span>
