39107989553d8b1a816d0fd821165a9429f03430
cesarbeloqui <beloqui.cesar@gmail.com>
2024-09-20 18:19:04 -0300

    sdfsd

--- src/components/LazyImage/LazyImage.jsx
+++ src/components/LazyImage/LazyImage.jsx
@@ -1,45 +1,41 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { startLoading, stopLoading } from "./redux/loadingSlice";
-import imagesData from "../../rize.json"
+import imagesData from "../../rize.json";
 import { removeLastPartAndExtractFileName } from "./utils";
+
 const LazyImage = ({ src, alt, className, onLoad }) => {
-  const { directory, fileName, fileWithExtension } = removeLastPartAndExtractFileName(src)
+  const { directory, fileName, fileWithExtension } = removeLastPartAndExtractFileName(src);
   const dispatch = useDispatch();
   const [imageSizes, setImageSizes] = useState([]);
-  const [srcSet, setSrcSet] = useState("")
-
+  const [srcSet, setSrcSet] = useState("");
   const [loaded, setLoaded] = useState(false);
+
+  // Cargar dimensiones de la imagen desde el archivo JSON
   useEffect(() => {
-    // Usamos directamente el objeto importado
     if (imagesData && imagesData[fileWithExtension]) {
       setImageSizes(imagesData[fileWithExtension].dimensions);
     }
-  }, []);
+  }, [fileWithExtension]);
+
+  // Construir el srcSet una vez que se carguen las dimensiones
   useEffect(() => {
-    /* 
-    imageSizes = [
-    "295×197",
-    "1060×706",
-    "1460×973",
-    "1780×1186",
-    "2048×1364"
-  ]
-    */
     if (imageSizes.length > 0) {
-      const srcSet = imageSizes.map((size, index) => {
-        // extraer extension del archivo:
-        const extension = fileWithExtension.split(".")[1]
-        const [width, height] = size.split("×").map(Number);
+      const newSrcSet = imageSizes.map((size) => {
+        const extension = fileWithExtension.split(".")[1];
+        const [width] = size.split("×").map(Number);
         return `${directory}/${fileName}/${width}.${extension} ${width}w`;
       }).join(", ");
-      setSrcSet(srcSet);
+      setSrcSet(newSrcSet);
     }
-  }, [imageSizes]);
+  }, [imageSizes, directory, fileName, fileWithExtension]);
 
+  // Manejar la carga de la imagen
   useEffect(() => {
+    if (!src) return;
+
     dispatch(startLoading({ src, alt }));
+
     const img = new Image();
     img.src = src;
     img.onload = () => {
@@ -47,17 +43,26 @@ const LazyImage = ({ src, alt, className, onLoad }) => {
       dispatch(stopLoading({ src, alt }));
       if (onLoad) onLoad();
     };
+
     return () => {
       dispatch(stopLoading({ src, alt }));
     };
-  }, [src]);
+  }, [src, alt, dispatch, onLoad]);
 
   return (
     <>
       {!loaded && <></>}
-      {/* Muestra el componente de carga mientras la imagen se carga */}
-      {loaded && <img src={src} alt={alt} className={className} srcSet={srcSet} sizes={imagesData[fileWithExtension]?.sizes} />}
+      {loaded && (
+        <img
+          src={src}
+          alt={alt}
+          className={className}
+          srcSet={srcSet} // Aseguramos que srcSet esté aquí
+          sizes={imagesData[fileWithExtension]?.sizes}
+        />
+      )}
     </>
   );
 };
+
 export default LazyImage;
