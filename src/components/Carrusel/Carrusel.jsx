2ecbc53d4d481ba05bc713082fc7c3a6cb496aee
cesarbeloqui <beloqui.cesar@gmail.com>
2024-09-20 19:51:11 -0300

    dfsdfsd

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -10,23 +10,23 @@ import useLocalizedContent from "@/hooks/useLocalizedContent";
 import ArrowLeft from "@icons/arrowLeft.svg?react";
 import ArrowRight from "@icons/arrowRight.svg?react";
 import Autoplay from "embla-carousel-autoplay";
-import image1 from "@carrusel/1.jpg";
-import image2 from "@carrusel/2.jpg";
-import image3 from "@carrusel/3.jpg";
-import image4 from "@carrusel/4.jpg";
-import image5 from "@carrusel/5.jpg";
-import image6 from "@carrusel/6.jpg";
-import image7 from "@carrusel/7.jpg";
+import image1 from "@carrusel/carrusel-1.jpg";
+import image2 from "@carrusel/carrusel-2.jpg";
+import image3 from "@carrusel/carrusel-3.jpg";
+import image4 from "@carrusel/carrusel-4.jpg";
+import image5 from "@carrusel/carrusel-5.jpg";
+import image6 from "@carrusel/carrusel-6.jpg";
+import image7 from "@carrusel/carrusel-7.jpg";
 import LazyImage from "../LazyImage/LazyImage";
 
 const images = [
     image1,
-        image2,
-        image3,
-        image4,
-        image5,
-        image6,
-        image7
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7
 ];
 
 function* pairGenerator(array1, array2) {
@@ -63,14 +63,14 @@ export default function FullscreenCarousel() {
                                         <LazyImage src={image} alt={`Slide ${index + 1}`} className="object-cover h-full w-full" />
                                     </div>
                                     <a href={slide.link} target="_blank" rel="noopener noreferrer" className="block">
-                                    <div className="relative z-10 flex h-[30vh] md:w-[50vw] flex-col items-center justify-center text-center">
-                                        <h1 className="text-xl md:text-3xl font-cutive text-celeste mb-0">
-                                            {slide.content}
-                                        </h1>
-                                        <p className="text-xl md:text-lg font-montserrat italic text-gris mt-0 ">
-                                            {slide.sub}
-                                        </p>
-                                    </div>
+                                        <div className="relative z-10 flex h-[30vh] md:w-[50vw] flex-col items-center justify-center text-center">
+                                            <h1 className="text-xl md:text-3xl font-cutive text-celeste mb-0">
+                                                {slide.content}
+                                            </h1>
+                                            <p className="text-xl md:text-lg font-montserrat italic text-gris mt-0 ">
+                                                {slide.sub}
+                                            </p>
+                                        </div>
 
                                     </a>
 
