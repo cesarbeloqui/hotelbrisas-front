922fb774b3969a027ab65f41cc68d4b635959df8
cesarbeloqui <beloqui.cesar@gmail.com>
2024-09-20 23:31:37 -0300

    sin cagarla

--- src/components/LazyImage/LazyImage.jsx
+++ src/components/LazyImage/LazyImage.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { startLoading, stopLoading } from './redux/loadingSlice';
 import imagesData from '../../rize.json';
 import { removeLastPartAndExtractFileName } from './utils';
-
-
-
 
 const LazyImage = ({ src, alt, className, onLoad }) => {
   const [isSupported, setisSupported] = useState(true);
@@ -16,80 +12,100 @@ const LazyImage = ({ src, alt, className, onLoad }) => {
   const dispatch = useDispatch();
   const [imageSizes, setImageSizes] = useState([]);
   const [srcSet, setSrcSet] = useState('');
-
   const [loaded, setLoaded] = useState(false);
+
+  // Detección de soporte para WebP
   useEffect(() => {
     const testWebP = () => {
       const canvas = document.createElement('canvas');
       canvas.width = 1;
       canvas.height = 1;
-
-      const isSupported = canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
-      return isSupported;
+      return canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
     };
     setisSupported(testWebP());
   }, []);
+
+  // Cargar tamaños de imágenes desde el archivo JSON
   useEffect(() => {
-    // Usamos directamente el objeto importado
     if (imagesData && imagesData[fileWithExtension]) {
       setImageSizes(imagesData[fileWithExtension].dimensions);
     }
-  }, []);
+  }, [fileWithExtension]);
+
+  // Generar srcSet para las imágenes
   useEffect(() => {
     if (imageSizes.length > 0) {
       const srcSet = imageSizes
-        .map((size, index) => {
-          // extraer extension del archivo:
+        .map((size) => {
           const extension = fileWithExtension.split('.')[1];
-          const [width, height] = size.split('×').map(Number);
-
+          const [width] = size.split('×').map(Number);
           const fileNameWithoutNumber = fileName.split('-')[0];
 
-          if (directory.includes(fileNameWithoutNumber)) {
-            return `${directory}/${fileName}/${width}.${extension} ${width}w`;
-          } else {
-            return `${directory}/${fileNameWithoutNumber}/${fileName}/${width}.${extension} ${width}w`;
-          }
+          // Generar la ruta correcta según la estructura de directorios
+          const path = directory.includes(fileNameWithoutNumber)
+            ? `${directory}/${fileName}/${width}.${extension}`
+            : `${directory}/${fileNameWithoutNumber}/${fileName}/${width}.${extension}`;
+
+          console.log('Ruta generada:', path); // Verificación de la ruta generada
+
+          return `${path} ${width}w`;
         })
         .join(', ');
+
       const srcSetWebp = srcSet.replace(/\.(jpg|png)/g, '.webp');
-      const srcWebp = src.replace(/\.(jpg|png)/g, '.webp')
-      console.log(srcWebp);
-      setSrcWebp(srcWebp)
+      const srcWebp = src.replace(/\.(jpg|png)/g, '.webp');
+
+      setSrcWebp(srcWebp);
       setSrcSetWebp(srcSetWebp);
       setSrcSet(srcSet);
     }
-  }, [imageSizes]);
+  }, [imageSizes, directory, fileName, fileWithExtension]);
 
+  // Manejo de la carga de imágenes
   useEffect(() => {
     dispatch(startLoading({ src, alt }));
+
     const img = new Image();
     img.src = src;
+
     img.onload = () => {
       setLoaded(true);
       dispatch(stopLoading({ src, alt }));
       if (onLoad) onLoad();
     };
+
+    img.onerror = () => {
+      console.error(`Error al cargar la imagen: ${src}`);
+      dispatch(stopLoading({ src, alt }));
+    };
+
     return () => {
       dispatch(stopLoading({ src, alt }));
     };
-  }, [src]);
+  }, [src, alt, dispatch, onLoad]);
 
   return (
     <>
       {!loaded && <></>}
-      {/* Muestra el componente de carga mientras la imagen se carga */}
       {loaded && srcSet !== '' && (
-        <img
-          src={isSupported ? srcWebp : src}
-          alt={alt}
-          className={className}
-          srcSet={isSupported ? srcSetWebp : srcSet}
-          sizes={imagesData[fileWithExtension]?.sizes}
-
-        />
+        <picture className={className}>
+          {/* Si el navegador soporta WebP, se usará este source */}
+          {isSupported && (
+            <source srcSet={srcSetWebp} sizes={imagesData[fileWithExtension]?.sizes} type="image/webp" />
+          )}
+          {/* Imagen original si no se soporta WebP */}
+          <img
+            loading="lazy"
+            src={src}
+            alt={alt}
+            className={className}
+            srcSet={srcSet}
+            sizes={imagesData[fileWithExtension]?.sizes}
+          />
+        </picture>
       )}
     </>
   );
 };
+
 export default LazyImage;
